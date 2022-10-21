731fd2f9a54311f7b916b2cd2001e3d5ab2d9d16
kamron-dev <109903041+kamron-dev@users.noreply.github.com>
2022-10-21 15:43:35 +0500

    Add files via upload

---
+++ sample button java.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let course_button = document.querySelector("#button_cs50");
+    course_button.addEventListener("click", function() {
+        window.open("https://cs50.harvard.edu/x/2022/", '_blank');
+    })
+
+});
